2b75e5641d2b6506805082abffe5ce04802b3e50
Faceless <146445263+TheRealFaceless@users.noreply.github.com>
2024-02-20 20:40:04 +0100

    Modified build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,26 +7,18 @@ group = "net.faceless"
 version = "1.0"
 description = "Simple Library"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
 repositories {
-    mavenCentral()
-    mavenLocal()
     maven {
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
+    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
 publishing {
     publications {
         create<MavenPublication>("maven") {
